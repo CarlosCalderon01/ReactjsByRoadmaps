84fef66d75a7a69daf790106fefe25f29558190e
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-12-04 08:48:56 -0500

    Fix project add SWC, add  new page about

---
+++ src/About.jsx
@@ -0,0 +1,25 @@
+function About() {
+    return (
+      <>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <h3>creando botones</h3>
+            <button>
+                Botton1
+            </button>
+            <button>
+                Botton2
+            </button>
+            <button>
+                Botton3
+            </button>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </>
+    )
+}
+  
+export default About
+  
