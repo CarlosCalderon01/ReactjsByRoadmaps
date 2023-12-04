d7ded530c27299127d149ca5bb5c9e7f8b9557ab
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-12-04 18:18:29 -0500

    Fix Comments

--- src/main.jsx
+++ src/main.jsx
@@ -13,16 +13,57 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 )
 
 /*
-  Elemento DOM --> document.getElementById('root')
-  tronco del arbol --> ReactDOM.createRoot
-  Todo lo que este dentro de la App son los elementos
+  // ----- // ----- // ----- // METHOD1 // ----- // ----- // ----- //
+  
+  Explicacion -->
 
-  en react no se puede renderizar html.
-    primero debo crear el elemento luego renderizarlo
+    Elemento DOM --> document.getElementById('root')
+    tronco del arbol --> ReactDOM.createRoot
+    Root --> en react se generan un arbol de elementos
+    Todo lo que este dentro de la App son los elementos
+      Como Especificar atributos:
+      ejemplo --> <button data-id= '123'
 
-  Root --> en react se generan un arbol de elementos
+    REACT --> no se puede renderizar html.
+      primero debo crear el elemento luego renderizarlo
 
-    const appDomElement = document.getElementById('app)
-    const root = ReactDOM.createRoot(appDomElement)
+    Que es JSX --> una extencion de ESCRIPT basada en XML declarativa.
+      (lenguaje de javascript para crear elementos en html)
+        recuerda no es HTML es JSX que es javascript convertido a html
+
+  // ----- // ----- // ----- // METHOD1 // ----- // ----- // ----- //
+
+  Vocabulary -->
+
+    Expreciones -->
+      es poner argum, var entre corchetes {}
+
+    Componente -->
+      - Es una funcion que devuelve un elemento.
+      - Los componentes tienen que ser PascalCase.
+
+*/
+
+/*
+
+  // ----- // ----- // ----- // METHOD1 // ----- // ----- // ----- //
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+  )
+
+  // ----- // ----- // ----- // METHOD2 // ----- // ----- // ----- //
+
+  const appgetElementById = document.getElementById('root')
+  const root = ReactDOM.createRoot(appgetElementById)
+  root.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+  )
+
+  // ----- // ----- // ----- // ----- // ----- // ----- // ----- //
 
 */
