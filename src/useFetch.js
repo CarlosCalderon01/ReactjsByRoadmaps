911a9f0e37105a8b60ff03bf519e309935c3752f
carlos.calderon01 <carlos.calderon01@outlook.com>
2023-12-09 17:09:46 -0500

    Create hook for Loading

--- src/useFetch.js
+++ src/useFetch.js
@@ -3,12 +3,17 @@ import { useState, useEffect } from "react";
 // const url = "https://jsonplaceholder.typicode.com/users"
 
 export function useFetch(url) {
+
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    setLoading(true) // true por defecto
     fetch(url)
       .then((Response) => Response.json())
-      .then((data) => setData(data));
+      .then((data) => setData(data))
+      .finally(() => setLoading(false)) // Dentro de la asyncronia
   }, []);
 
-  return {data}
+  return {data, loading}
 }
