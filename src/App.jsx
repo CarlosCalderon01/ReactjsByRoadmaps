d3a6bff11c0d7c0d8170d4297a5ce21c0925cac5
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-12-20 18:21:58 -0500

    Traditional Method Fetch useEffect and useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useFetch } from "./Components/FetchFiles/useFetch";
 import "./App.css";
 
 function App() {
-  const {data, loading, error} = useFetch("https://jsonplaceholder.typicode.com/users")
+  const {data, loading, error, handleCancelRequest} = useFetch("https://jsonplaceholder.typicode.com/users")
 
   return (
     <div className="App">
       <div className="title">
         <h1>Getch like a pro</h1>
+      </div>
+      <div>
+        <button onClick = {handleCancelRequest}>Cancel Request!</button>
       </div>
       <div className="card">
         <ul>
--- src/Components/FetchFiles/useFetch.js
+++ src/Components/FetchFiles/useFetch.js
@@ -5,26 +5,57 @@ export function useFetch(url) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [controller, setController] = useState(null);
 
   useEffect(() => {
-    setLoading(true)
-    fetch(url)
+    const abortController = new AbortController();
+    setController(abortController);
+    setLoading(true);
+    fetch(url, { signal: abortController.signal }) // rastreador a la petecion para poder controlarlo
       .then((Response) => Response.json()) // Recibe promesa => transforma en JSON
       .then((data) => setData(data)) // Recibe data => La setea y incluye en el estado.
-      .catch((error) => setError(error)) // Captura el error => La setea y incluye en el estado.
+      .catch((error) => {
+        if(error.name === "AbortError"){
+          console.log("Request Cancelled");
+        } else {
+          setError(error)
+        }
+      }  
+    ) // Captura el error => La setea y incluye en el estado.
       .finally(() => setLoading(false)) // Cambia el estado de carga a falso, Con o Sin Exito
+
+    return () => abortController.abort(); // Se ejecuta Auto Cuando componente sea desmontado
   }, [url]);
   // [] o [url]); Sin url Ejecuta 1 vez, Cuando se monte el componente. Con url Ejecuta cada vez que cambie url
   
+  const handleCancelRequest = () => {
+    if (controller) {
+      controller.abort();
+      setError("request Cancelled");
+    }
+  }
+
   // Retornar Los datos de los Hooks.
-  return {data, loading, error}
+  return {data, loading, error, handleCancelRequest}
 }
 
 /*
-  Explicacion:
-    1- function useFetch que recibe una url como parametro o dependencia.
-    2- Creamos 3 hooks para los datos, el estado de carga, los errores.
-  Bibliografia -->
-    Cómo Consumir APIs en REACT como un PROFESIONAL By Carlos Azaustre
-    https://youtu.be/6u1RHUoXIPI?si=BXX5XvhnzgmpLFwG
+
+Notes -->
+  + Consumo TRADICIONAL de un fetch, existe una mejor manera que se llama "Render-as-you-Fetch"
+
+Features -->
+  + Renderizado con UseEffect y useState --> Renderiza multiples veces
+  + maneja de errores
+  + Puede abortar las promesas antes de recibirlas
+
+Good Practice -->
+  + "Render-as-you-Fetch" --> que se renderice Unicamente cuando se haga el Feching de datos.
+
+Explained -->
+  + creamos 4 hooks para los datos, errores, estado de carga, cancelar promesas
+
+Bibliography -->
+  + Cómo Consumir APIs en REACT como un PROFESIONAL By Carlos Azaustre
+      https://youtu.be/6u1RHUoXIPI?si=BXX5XvhnzgmpLFwG
 */
