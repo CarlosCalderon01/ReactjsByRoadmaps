86509b63436d9bbf003bf382da57a1721978d223
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-12-23 19:55:02 -0500

    learning use only Eslint With a professional

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,48 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect' // O '16.8', '17.0', etc. según tu versión específica
+    }
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    // 'react/react-in-jsx-scope': RULES.OFF // como SOBRE ESCRIBIR mi regla por ensima de ESLINT
+  }
+}
+
+/*
+
+  module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
@@ -18,3 +62,5 @@ module.exports = {
     ],
   },
 }
+
+*/
