943986abb268331d43de308cf91526265fd6287f
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-12-26 20:43:16 -0500

    add React Router6 and make SPA

---
+++ src/App5.jsx
@@ -0,0 +1,37 @@
+// dependency - third
+import React from 'react'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom' // Cual es el path en elq ue tiene que renderizar cierto elemento
+// dependency - Local
+import './App.css'
+import { Homes } from './page/Homes'
+import { Notes } from './page/Notes'
+import { Users } from './page/Users'
+import { About } from './page/About'
+
+function App5 () {
+  return (
+    <div className="App">
+        <BrowserRouter>
+            <header>
+                <h1>menu</h1>
+                <nav>
+                    <ul>
+                        <li><Link to='/'>Homes</Link></li>
+                        <li><Link to='/notes'>Notes</Link></li>
+                        <li><Link to='/users'>Users</Link></li>
+                        <li><Link to='/about'>About</Link></li>
+                    </ul>
+                </nav>
+            </header>
+                <Routes>
+                    <Route path='/' element={<Homes />} />
+                    <Route path='/notes' element={<Notes />} />
+                    <Route path='/users' element={<Users />} />
+                    <Route path='/about' element={<About />} />
+                </Routes>
+        </BrowserRouter>
+    </div>
+  )
+}
+
+export default App5
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,12 @@ import './index.css'
 // import App1 from "./App1.jsx";
 // import App2 from "./App2.jsx";
 // import App3 from "./App3.jsx"
-import App4 from './App4.jsx'
+// import App4 from './App4.jsx'
+import App5 from './App5.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App4 />
+    <App5 />
   </React.StrictMode>
 )
 
