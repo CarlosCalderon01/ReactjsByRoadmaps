b48fe8f5647590a05e50911ca6ae65b9c5443856
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-12-28 17:13:40 -0500

    return App main

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // dependency - Local
-import './index.css'
-// import App from "./App.jsx";
-// import App1 from "./App1.jsx";
-// import App2 from "./App2.jsx";
-// import App3 from "./App3.jsx"
-// import App4 from './App4.jsx'
-import App5 from './App5.jsx'
+
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App5 />
+    <App />
   </React.StrictMode>
 )
 
