654409a5ea3a5162266cc17bbdfb4ed2371dd79f
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-12-28 17:14:26 -0500

    new comp Menu and add in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
 // dependency - third
 import React from 'react'
 // dependency - Local
-import { useFetch } from './Components/FetchFiles/useFetch'
 import './App.css'
+import Menu from './Components/Menu'
 
 function App () {
-  const { data, loading, error, handleCancelRequest } = useFetch('https://jsonplaceholder.typicode.com/users')
-
   return (
     <div className="App">
-      <div className="title">
-        <h1>Getch like a pro</h1>
-      </div>
-      <div>
-        <button onClick = {handleCancelRequest}>Cancel Request!</button>
-      </div>
-      <div className="card">
-        <ul>
-          {error && <li>error: {error}</li> }
-          {loading && <li>Loading...  </li> }
-          {data?.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      </div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Menu>
+      </Menu>
     </div>
   )
 }
---
+++ src/App0.jsx
@@ -0,0 +1,31 @@
+// dependency - third
+import React from 'react'
+// dependency - Local
+import { useFetch } from './Components/FetchFiles/useFetch'
+import './App.css'
+
+function App () {
+  const { data, loading, error, handleCancelRequest } = useFetch('https://jsonplaceholder.typicode.com/users')
+
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Getch like a pro</h1>
+      </div>
+      <div>
+        <button onClick = {handleCancelRequest}>Cancel Request!</button>
+      </div>
+      <div className="card">
+        <ul>
+          {error && <li>error: {error}</li> }
+          {loading && <li>Loading...  </li> }
+          {data?.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
