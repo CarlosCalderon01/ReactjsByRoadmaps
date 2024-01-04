4eab928a99ade704c9e868a9d8dff4b9a7517d79
NewLine2012xD <Carlos.Calderon01@outlook.com>
2024-01-04 15:39:41 -0500

    add axios and two more, add new componente menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // dependency - third
 import React from 'react'
 // dependency - Local
-import './App.css'
-import Menu from './Components/Menu'
+import './index.css'
+// import Menu from './Components/Menu'
+// import './App.css'
+import VerticalMenu from '../src/Components/VerticalMenu'
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Menu></Menu>
+      <VerticalMenu></VerticalMenu>
     </div>
   )
 }
